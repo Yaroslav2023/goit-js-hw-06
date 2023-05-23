c2aea5992b96532ca5e66e3ae5bced646eca2c59
Yaroslav2023 <yishevchenko@ukr.net>
2023-05-24 00:20:05 +0300

    task-5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const refs = {
+input: document.querySelector("#name-input"),
+span: document.querySelector("#name-output"),
+};
+
+refs.input.addEventListener('input', onInputChange );
+
+
+
+
+function onInputChange(event) {
+
+    refs.span.textContent = event.currentTarget.value;
+}
